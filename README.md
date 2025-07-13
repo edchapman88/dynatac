bf948ace4ee79583e2fb454e74fc0a67893c5868
Ed Chapman - Turing <edchapmanelc@gmail.com>
2025-07-13 18:12:37 +0100

    pivot to esp32s3

--- README.md
+++
@@ -1,5 +0,0 @@
-## ARM Assembly
-- **STP** : store a pair of registers
-- **SUM** : subtract
-- **MRS** : move system resgister to general purpose register
-- **MOV** : `MOV dest src`
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,10 @@
-#![no_std]
-// 'binary' crates in Rust must include a function called 'main' to define the executable entry point unless the `no_main` attribute may be applied at the crate level.
-#![no_main]
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
 
-use aarch64_cpu::asm;
-use dynatac::entry_point;
-use dynatac::exception;
-use dynatac::println;
-use dynatac::state;
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
 
-mod boot;
-
-/// Pause execution on the core.
-#[inline(always)]
-pub fn halt_cpu() -> ! {
-    loop {
-        asm::wfe()
-    }
-}
-
-fn panic_prevent_reenter() {
-    use core::sync::atomic::{AtomicBool, Ordering};
-    static PANIC_IN_PROGRESS: AtomicBool = AtomicBool::new(false);
-    if !PANIC_IN_PROGRESS.load(Ordering::Relaxed) {
-        PANIC_IN_PROGRESS.store(true, Ordering::Relaxed);
-        return;
-    }
-    halt_cpu();
-}
-
-// #[panic_handler] is used to define the behavior of the Rust `panic!` macro (a panic is a fatal exception) in #![no_std] applications.
-// https://doc.rust-lang.org/nomicon/panic-handler.html
-#[panic_handler]
-fn panic_handler(info: &core::panic::PanicInfo) -> ! {
-    panic_prevent_reenter();
-    println!("{}", info);
-    halt_cpu();
-}
-
-unsafe fn kernel_init() -> ! {
-    exception::handling_init();
-
-    // Announce conclusion of the kernel_init() phase.
-    state::state_manager().transition_to_single_core_main();
-
-    entry_point();
-    halt_cpu();
+    log::info!("Hello, world!");
 }
